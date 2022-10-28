f6ee2f3d267277b9790a85a912d4c1f4c95825d9
Sunny <in.wise.nature@gmail.com>
2022-10-28 22:40:23 +0900

    refactor: apply base url + fix eslint error

--- front/src/layouts/Layout_WithoutAppBar.jsx
+++ front/src/layouts/Layout_WithoutAppBar.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 export default function Layout_WithoutAppBar() {
-	return (
-		<Container>
-			<Outlet />
-		</Container>
-	);
+  return (
+    <Container>
+      <Outlet />
+    </Container>
+  );
 }
 
 const Container = styled.div`
-	width: 1200px;
-	margin: 0 auto;
-	height: 100vh;
+  width: 1200px;
+  margin: 0 auto;
+  height: 100vh;
 `;
--- front/src/routes/index.jsx
+++ front/src/routes/index.jsx
@@ -1,18 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import Layout from '../layouts/Layout';
-import Layout_WithoutBar from '../layouts/Layout_WithoutAppBar';
-import { Home, Login, Test } from '../pages';
+import Layout from 'layouts/Layout';
+import LayoutWithoutBar from 'layouts/Layout_WithoutAppBar';
+import { Home, Login, Test } from 'pages';
 
 export default function index() {
-	return (
-		<Routes>
-			<Route element={<Layout />}>
-				<Route path='/' element={<Home />} />
-				<Route path='/test' element={<Test />} />
-			</Route>
-			<Route element={<Layout_WithoutBar />}>
-				<Route path='/login' element={<Login />} />
-			</Route>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<Test />} />
+      </Route>
+      <Route element={<LayoutWithoutBar />}>
+        <Route path='/login' element={<Login />} />
+      </Route>
+    </Routes>
+  );
 }
