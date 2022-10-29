b89969c35ba49f489f6fe5f7fef0e14bedb2c824
ReturnReason <reasonz@daum.net>
2022-10-30 03:43:23 +0900

    feat: Button 컴포넌트 close #24

---
+++ front/src/components/Button.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+export default function Button({ children, isOutline, btnSize, customSize }) {
+  return (
+    <ButtonStyles
+      isOutline={isOutline}
+      btnSize={btnSize}
+      customSize={customSize}
+    >
+      {children}
+    </ButtonStyles>
+  );
+}
+
+const ButtonStyles = styled.button`
+  background: #9747ff;
+  border: 3px solid #9747ff;
+  color: #fff;
+  cursor: pointer;
+  border-radius: 10px;
+  font-size: 16px;
+  font-weight: 700;
+  padding: 8px;
+  width: ${(props) => props.customSize};
+  transition: all 0.2s;
+
+  ${(props) =>
+    props.isOutline &&
+    css`
+      background: #fff;
+      color: #9747ff;
+    `}
+
+  ${(props) => {
+    if (props.btnSize === 'small') {
+      return css`
+        width: 100px;
+      `;
+    } else if (props.btnSize === 'middle') {
+      return css`
+        width: 200px;
+      `;
+    } else if (props.btnSize === 'large') {
+      return css`
+        width: 400px;
+      `;
+    }
+  }};
+
+  &:hover {
+    background: ${(props) => (props.isOutline ? '#E9DCFF50' : '#843DE0')};
+    border: ${(props) => !props.isOutline && '3px solid #843DE0'};
+  }
+`;
