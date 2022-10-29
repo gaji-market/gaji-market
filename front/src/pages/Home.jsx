f499f5292662d2f0a483a938eb9930b231ea2907
ReturnReason <reasonz@daum.net>
2022-10-30 03:44:55 +0900

    refactor: 컴포넌트 테스트 적용

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import Button from 'components/Button';
 import Card from 'components/Card';
 import React from 'react';
 
@@ -6,6 +7,18 @@ export default function Home() {
     <>
       <div>Home</div>
       <Card title='title' price='123,000' area='서울시 동작구' likes='0' />
+      <Button isOutline={false} btnSize='small'>
+        기본 버튼
+      </Button>
+      <Button isOutline={true} btnSize='middle'>
+        아웃라인 버튼
+      </Button>
+      <Button isOutline={false} btnSize='large'>
+        버튼 사이즈 테스트
+      </Button>
+      <Button isOutline={true} customSize='700px'>
+        버튼 사이즈 커스텀
+      </Button>
     </>
   );
 }
