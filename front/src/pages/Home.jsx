952a998a6a3c70dba4efc0b57d7bef2ae62d0248
ReturnReason <reasonz@daum.net>
2022-10-31 10:35:28 +0900

    refactor: 버튼 테스트 코드

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -7,17 +7,12 @@ export default function Home() {
     <>
       <div>Home</div>
       <Card title='title' price='123,000' area='서울시 동작구' likes='0' />
-      <Button isOutline={false} btnSize='small'>
-        기본 버튼
-      </Button>
-      <Button isOutline={true} btnSize='middle'>
-        아웃라인 버튼
-      </Button>
-      <Button isOutline={false} btnSize='large'>
-        버튼 사이즈 테스트
-      </Button>
-      <Button isOutline={true} customSize='700px'>
-        버튼 사이즈 커스텀
+      <Button>basic</Button>
+      <Button btnSize='sm'>sm</Button>
+      <Button isOutline={true}>outline</Button>
+      <Button btnSize='lg'>lg</Button>
+      <Button isOutline={true} customSize='900px'>
+        커스텀
       </Button>
     </>
   );
