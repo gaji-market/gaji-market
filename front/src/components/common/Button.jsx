c6e7162016d45f59cc2241051e4a2325763e7730
ReturnReason <reasonz@daum.net>
2022-10-31 11:36:56 +0900

    refactor: 버튼 스타일 재정의 및 코드 수정

--- front/src/components/common/Button.jsx
+++ front/src/components/common/Button.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { PRIMARY_COLOR, WHITE_COLOR, SUB_COLOR } from './commonColor';
 
 export default function Button({
   children,
   customSize,
   isOutline,
   btnSize = 'md',
+  isDarkColor = false,
 }) {
   return (
     <ButtonStyles
       isOutline={isOutline}
       btnSize={btnSize}
       customSize={customSize}
+      isDarkColor={isDarkColor}
     >
       {children}
     </ButtonStyles>
@@ -19,9 +22,9 @@ export default function Button({
 }
 
 const ButtonStyles = styled.button`
-  background: #9747ff;
-  border: 3px solid #9747ff;
-  color: #fff;
+  background: ${PRIMARY_COLOR};
+  border: 3px solid ${PRIMARY_COLOR};
+  color: ${WHITE_COLOR};
   cursor: pointer;
   border-radius: 10px;
   font-size: 16px;
@@ -30,10 +33,26 @@ const ButtonStyles = styled.button`
   transition: all 0.2s;
 
   ${(props) =>
+    props.isDarkColor &&
+    css`
+      background: ${SUB_COLOR};
+      border: 3px solid ${SUB_COLOR};
+    `}
+
+  ${(props) =>
     props.isOutline &&
     css`
-      background: #fff;
-      color: #9747ff;
+      background: ${WHITE_COLOR};
+      color: ${PRIMARY_COLOR};
+    `}
+
+  ${(props) =>
+    props.isOutline &&
+    props.isDarkColor &&
+    css`
+      background: ${WHITE_COLOR};
+      color: ${SUB_COLOR};
+      border: 3px solid ${SUB_COLOR};
     `}
 
   ${(props) => {
@@ -55,7 +74,17 @@ const ButtonStyles = styled.button`
   width: ${(props) => props.customSize};
 
   &:hover {
+    // 아웃라인 버튼
     background: ${(props) => (props.isOutline ? '#E9DCFF50' : '#843DE0')};
     border: ${(props) => !props.isOutline && '3px solid #843DE0'};
+
+    // 다크그레이 버튼
+    background: ${(props) => props.isDarkColor && '#222222'};
+    border: ${(props) => props.isDarkColor && '3px solid #222222'};
+    color: ${(props) => props.isDarkColor && `${WHITE_COLOR}`};
+
+    // 아웃라인 + 다크그레이 버튼
+    background: ${(props) => props.isDarkColor && props.isOutline && '#F3F3F3'};
+    color: ${(props) => props.isDarkColor && props.isOutline && `${SUB_COLOR}`};
   }
 `;
