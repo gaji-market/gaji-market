82ffd50d34aa1375a134516ab57b165c6d12d532
ReturnReason <reasonz@daum.net>
2022-11-04 08:21:25 +0900

    feat: ProductView 모듈 추가

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as Test } from './Test';
 export { default as Login } from './Login';
 export { default as SliceTest } from './SliceTest';
 export { default as SignUp } from './SignUp';
+export { default as ProductView } from './ProductView';
