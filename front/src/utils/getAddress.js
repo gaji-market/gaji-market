f17e26ab8043395e921dc8b8e1f6397e5ac6bb72
vjvl95 <vjvl95@naver.com>
2022-11-04 00:44:34 +0900

    fix: 확장자 수정

---
+++ front/src/utils/getAddress.js
@@ -0,0 +1,15 @@
+export default function getAddress(data) {
+  let fullAddress = data.address;
+  let extraAddress = '';
+
+  if (data.addressType === 'R') {
+    if (data.bname !== '') {
+      extraAddress += data.bname;
+    }
+    if (data.buildingName !== '') {
+      extraAddress += extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
+    }
+    fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
+  }
+  return fullAddress;
+}
