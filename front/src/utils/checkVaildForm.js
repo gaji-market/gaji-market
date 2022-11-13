4f8d70fd6ef9c7e3510645e4c4a52712894c497d
vjvl95 <vjvl95@naver.com>
2022-11-13 20:14:04 +0900

    feat: vaild 체크를 위한 정규식 함수 추가

---
+++ front/src/utils/checkVaildForm.js
@@ -0,0 +1,14 @@
+function isVaildId(id) {
+  var idRule = /^[A-Za-z0-9]{6,12}$/;
+  console.log(id);
+  return idRule.test(id);
+}
+function isVaildPassword(password) {
+  let pwRule = /^[A-Za-z0-9]{8,15}$/;
+  return pwRule.test(password);
+}
+function isETCVaild(address, addressDetail, birthday, gender) {
+  if (address !== '' && addressDetail !== '' && birthday !== '' && gender !== '') return true;
+  return false;
+}
+export { isVaildId, isVaildPassword, isETCVaild };
