ef819103e936909b44f10a1c584559e45fa15067
Sunny <in.wise.nature@gmail.com>
2022-11-13 18:44:41 +0900

    feat: add primary_var colors to common colors

--- front/src/components/common/commonColor.js
+++ front/src/components/common/commonColor.js
@@ -1,4 +1,5 @@
 export const PRIMARY_COLOR = '#9747ff';
+export const PRIMARY_VAR_COLOR = '#e9dcff';
 export const SECONDARY_COLOR = '#89C113';
 export const SUB_COLOR = '#353535';
 export const WHITE_COLOR = '#ffffff';
