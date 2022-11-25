8927a8995a1e654e0fec1f6e265dbb6aec0f8ce1
ReturnReason <reasonz@daum.net>
2022-11-26 01:14:04 +0900

    design(commonColor): SECONDARY_COLOR 색상 코드값 변경

--- front/src/components/common/commonColor.js
+++ front/src/components/common/commonColor.js
@@ -1,4 +1,4 @@
 export const PRIMARY_COLOR = '#9747ff';
-export const SECONDARY_COLOR = '#60df24';
+export const SECONDARY_COLOR = '#89C113';
 export const SUB_COLOR = '#353535';
 export const WHITE_COLOR = '#ffffff';
