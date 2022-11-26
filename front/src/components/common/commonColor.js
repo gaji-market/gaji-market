ac08a2263262a08390d9ff578d6c7353efa58360
vjvl95 <vjvl95@naver.com>
2022-11-26 19:22:58 +0900

    style: GRAY_COLOR 색상 변경

--- front/src/components/common/commonColor.js
+++ front/src/components/common/commonColor.js
@@ -2,4 +2,4 @@ export const PRIMARY_COLOR = '#9747ff';
 export const SECONDARY_COLOR = '#60df24';
 export const SUB_COLOR = '#353535';
 export const WHITE_COLOR = '#ffffff';
-export const GRAY_COLOR = '#cccccc';
+export const GRAY_COLOR = '#9a9a9a';
