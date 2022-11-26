3411ccaf7bcf14c0b724845e4940d0843b355f18
ReturnReason <reasonz@daum.net>
2022-11-26 23:58:19 +0900

    feat(InputTextBox): InputTextBox 컴포넌트 추가

---
+++ front/src/components/common/InputTextBox.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { PRIMARY_COLOR } from './commonColor';
+
+const TEMP_COLOR = '#aaa';
+
+export default function InputTextBox({
+  width,
+  type,
+  placeholder,
+  placeholderPosition = 'left',
+}) {
+  return (
+    <Container>
+      <Input
+        width={width}
+        type={type}
+        placeholder={placeholder}
+        placeholderPosition={placeholderPosition}
+      />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  margin-top: 8px;
+`;
+
+const Input = styled.input`
+  width : 200px;
+  width: ${({ width }) => width};
+
+  border-radius: 5px;
+  padding: 8px;
+  border: 2px solid ${TEMP_COLOR};
+  transition: all 0.2s;
+
+  &:focus {
+    outline: none;
+    border: 2px solid ${PRIMARY_COLOR};
+  }
+
+  &::placeholder {
+    color: ${TEMP_COLOR};
+    text-align: ${({ placeholderPosition }) => placeholderPosition}
+`;
