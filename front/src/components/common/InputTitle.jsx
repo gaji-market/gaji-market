a5a0d68b33dd5f486fcefbe5f3248576cc187f4c
ReturnReason <reasonz@daum.net>
2022-11-26 23:57:58 +0900

    feat(InputTitle): InputTitle 컴포넌트 추가

---
+++ front/src/components/common/InputTitle.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import { PRIMARY_COLOR } from './commonColor';
+
+export default function InputTitle({ title, subTitle, isRequired = false }) {
+  return (
+    <>
+      {isRequired && <Asterisk>*</Asterisk>}
+      <Title>{title}</Title>
+      {subTitle && <SubTitle>{subTitle}</SubTitle>}
+    </>
+  );
+}
+const Asterisk = styled.span`
+  font-weight: 900;
+  margin-right: 10px;
+  color: ${PRIMARY_COLOR};
+`;
+
+const Title = styled.span`
+  font-weight: 700;
+`;
+
+const SubTitle = styled.span`
+  margin-left: 8px;
+  font-size: 12px;
+  color: #aaa;
+`;
