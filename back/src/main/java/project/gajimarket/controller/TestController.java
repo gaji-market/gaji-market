57629c6bb90fd9d66e5fa2ed6d653dd7f8f566dd
Geondol <keedob@naver.com>
2022-11-27 21:45:02 +0900

    test: 배포 테스트 코드

--- back/src/main/java/project/gajimarket/controller/TestController.java
+++ back/src/main/java/project/gajimarket/controller/TestController.java
@@ -1,11 +1,13 @@
 package project.gajimarket.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/hello")
     public String test(){
         return "hello";
