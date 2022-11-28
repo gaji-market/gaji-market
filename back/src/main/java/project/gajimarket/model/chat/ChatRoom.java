533e8b8da5b51ac231a2d8d937afa60dcf628a87
HeeJooYoo <heejooyoo93@gmail.com>
2022-11-28 19:27:07 +0900

    feat: chat 구현 파일 생성

---
+++ back/src/main/java/project/gajimarket/model/chat/ChatRoom.java
@@ -0,0 +1,37 @@
+package project.gajimarket.model.chat;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.web.socket.WebSocketSession;
+import project.gajimarket.service.ChatService;
+import project.gajimarket.service.impl.ChatServiceImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class ChatRoom {
+    private String chatId;
+    private String name;
+    private Set<WebSocketSession> sessions = new HashSet<>();
+
+    @Builder
+    public ChatRoom(String chatId, String name) {
+        this.chatId = chatId;
+        this.name = name;
+    }
+
+    public void handlerActions(WebSocketSession session, ChatMessage chatMessage, ChatServiceImpl chatService) {
+        if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
+            sessions.add(session);
+            chatMessage.setMessage(chatMessage.getSenderId() + "님이 입장했습니다.");
+        }
+        sendMessage(chatMessage, chatService);
+
+    }
+
+    private <T> void sendMessage(T message, ChatServiceImpl chatService) {
+        sessions.parallelStream()
+                .forEach(session -> chatService.sendMessage(session, message));
+    }
+}
---
+++ back/src/main/java/project/gajimarket/service/impl/ChatServiceImpl.java
@@ -0,0 +1,55 @@
+package project.gajimarket.service.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import project.gajimarket.dao.ChatDAO;
+import project.gajimarket.model.chat.ChatRoom;
+import project.gajimarket.service.ChatService;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ChatServiceImpl implements ChatService {
+    private final ObjectMapper objectMapper;
+    private final ChatDAO chatDAO;
+    private Map<String, ChatRoom> chatRooms;
+
+    @PostConstruct
+    private void init() {
+        chatRooms = new LinkedHashMap<>();
+    }
+
+    public ChatRoom createChatRoom(String name) {
+        String randomId = UUID.randomUUID().toString();
+        ChatRoom chatRoom = ChatRoom.builder()
+                .chatId(randomId)
+                .name(name)
+                .build();
+        chatRooms.put(randomId, chatRoom);
+        return chatRoom;
+    }
+
+    public List<ChatRoom> getChatRoomList() {
+        return new ArrayList<>(chatRooms.values());
+    }
+
+    public ChatRoom getChatRoom(String chatId) {
+        return chatDAO.selectChatRoom(chatId);
+    }
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try{
+            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+}
