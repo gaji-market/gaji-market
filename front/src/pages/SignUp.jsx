e737e8f9b99700dd3503f95baee062bafd0af336
vjvl95 <vjvl95@naver.com>
2022-11-28 20:29:30 +0900

    fact: 셩별 및 달력 커스텀 작업

--- front/src/pages/SignUp.jsx
+++ front/src/pages/SignUp.jsx
@@ -8,6 +8,10 @@ import { isVaild } from 'utils/checkVaildForm';
 import InputTextBox from 'components/common/InputTextBox';
 import InputTitle from 'components/common/InputTitle';
 import { GRAY_COLOR, PRIMARY_COLOR } from 'components/common/commonColor';
+import { BsGenderMale } from 'react-icons/bs';
+import logo200 from '../assets/BasicLogo.svg';
+import man from '../assets/man.png';
+import woman from '../assets/woman.png';
 
 const DaumURL = 'https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
 const NICK_NAME_MIX_LENGTH = 4;
@@ -158,10 +162,10 @@ export default function SignUp() {
 
           <FlexItem margin={'50px'}>
             <Title margin={'10px'}>성별</Title>
-            <Raido type='radio' name='gender' value={'남자'} />
-            남자
-            <Raido type='radio' name='gender' value={'여자'} />
-            여자
+            <HiddenRadioButton id='man' type='radio' name='gender' value={'남자'} />
+            <RadioButton htmlFor='man' type={'man'}></RadioButton>
+            <HiddenRadioButton id='woman' type='radio' name='gender' value={'여자'} />
+            <RadioButton htmlFor='woman' type={'woman'}></RadioButton>
           </FlexItem>
         </FlexBox>
         <ButtonBox>
@@ -174,14 +178,18 @@ export default function SignUp() {
           )}
         </ButtonBox>
       </Form>
+      <CopyRight href='https://www.flaticon.com/kr/free-icons/' title='성별 아이콘'>
+        성별 아이콘 제작자: Vitaly Gorbachev - Flaticon
+      </CopyRight>
     </Container>
   );
 }
 const Container = styled.div`
   width: 700px;
   height: 800px;
-  margin: 50px auto;
-  box-shadow: 0px 0px 20px 1px ${GRAY_COLOR};
+  margin: 60px auto;
+  border-radius: 35px;
+  box-shadow: 0px 0px 30px 1px ${GRAY_COLOR};
   padding: 50px 100px;
 `;
 const Title = styled.div`
@@ -191,7 +199,33 @@ const Title = styled.div`
   margin-left: ${(props) => props.margin};
 `;
 const Date = styled.input`
-  width: 100px;
+  & {
+    padding-right: 15px;
+    position: relative;
+    background: url(${logo200}) no-repeat right center;
+    border-radius: 5px;
+  }
+  &:hover,
+  &:active {
+    border: 2px solid ${PRIMARY_COLOR};
+    border-radius: 5px;
+  }
+
+  &::-webkit-clean-button,
+  &::-webkit-inner-spin-button {
+    display: none;
+  }
+  &::-webkit-calendar-picker-indicator {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: transparent;
+    color: transparent;
+    cursor: pointer;
+  }
+  width: 125px;
   height: 20px;
 `;
 const SubTitle = styled.p`
@@ -204,7 +238,24 @@ const SubTitle = styled.p`
 const SignUpHead = styled.div`
   display: flex;
 `;
-const Raido = styled.input``;
+const RadioButton = styled.label`
+  display: inline-block;
+  width: 55px;
+  height: 80px;
+  border-radius: 20px;
+
+  background: ${(props) =>
+    props.type === 'man' ? `url(${man}) no-repeat` : `url(${woman}) no-repeat`};
+  //background: url(${woman}) no-repeat;
+  cursor: pointer;
+`;
+const HiddenRadioButton = styled.input`
+  display: none;
+  &:checked + ${RadioButton} {
+    transition: 0.7s;
+    transform: scale(1.3);
+  }
+`;
 const Line = styled.div`
   border-bottom: 1px solid #eeeeee;
   margin-top: 15px;
@@ -226,4 +277,11 @@ const ButtonBox = styled.div`
   margin-top: 30px;
   display: flex;
   justify-content: center;
+`;
+const CopyRight = styled.a`
+  position: fixed;
+  bottom: 0px;
+  right: 0px;
+  text-decoration: none;
+  color: #cccccc;
 `;
