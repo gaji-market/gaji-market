25b4bcbced5d36f748616fba85705ba71efef699
Sunny <in.wise.nature@gmail.com>
2022-12-02 19:13:56 +0900

    feat: add style to chat list

--- front/src/pages/Chat.jsx
+++ front/src/pages/Chat.jsx
@@ -1,76 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-import { PRIMARY_VAR_COLOR } from 'components/common/commonColor';
+import { PRIMARY_VAR_COLOR, WHITE_COLOR } from 'components/common/commonColor';
 import { css } from 'styled-components';
 
 export default function Chat() {
+  const [selectedChat, setSelectedChat] = useState(TEMP_LIST_ITEMS[0].username);
+
   return (
-    <FlexBox colGap='8px' padding='8px'>
-      <Container width='480px'>
-        <Header>
-          <h3>Chat</h3>
-        </Header>
+    <Wrapper>
+      <ChatList>
+        <Header>Chat</Header>
         <Body>
-          <ChatRoomItem>
-            <FlexBox xCenter={true} yCenter={true} padding='8px'>
-              <span>IMAGE</span>
-            </FlexBox>
-            <ChatContent>
-              <div>
-                <span>AVATAR</span>
-                <span>USER_ID</span>
-              </div>
-              <span>last message</span>
-            </ChatContent>
-            <FlexBox xCenter={true} yCenter={true} padding='8px'>
-              <span>2022-11-11</span>
-            </FlexBox>
-          </ChatRoomItem>
+          {TEMP_LIST_ITEMS.map((item, i) => (
+            <ChatItem
+              key={item.username}
+              className={selectedChat === item.username ? 'current' : ''}
+              onClick={() => setSelectedChat(item.username)}
+            >
+              <Box padding='8px' center='true'>
+                <ProductImg>
+                  {item.product && <img src={item.product} alt='product.jpg' />}
+                </ProductImg>
+              </Box>
+              <Box padding='8px' fill='true'>
+                <UserInfo>
+                  <Avatar>
+                    {item.avatar && <img src={item.avatar} alt='avatar.jpg' />}
+                  </Avatar>
+                  <Username>{item.username}</Username>
+                </UserInfo>
+                <LastMsg>{item.lastMsg}</LastMsg>
+              </Box>
+              <Box padding='8px' center='true'>
+                <UpdatedTime>{item.updatedAt}</UpdatedTime>
+              </Box>
+            </ChatItem>
+          ))}
         </Body>
-        <FlexBox direction='column'>
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-        </FlexBox>
-      </Container>
-      <Container>TEST</Container>
-    </FlexBox>
+      </ChatList>
+      <ChatMessage>
+        <Header>TITLE</Header>
+        <Body>BODY</Body>
+      </ChatMessage>
+    </Wrapper>
   );
 }
 
-const FlexBox = styled.div`
-  height: 100%;
+const TEMP_LIST_ITEMS = [
+  {
+    product: null,
+    avatar: null,
+    username: 'user1',
+    unread: 0,
+    lastMsg: 'hello, there',
+    updatedAt: '6:29 PM',
+  },
+  {
+    product: null,
+    avatar: null,
+    username: 'user2',
+    unread: 0,
+    lastMsg: 'hello, there',
+    updatedAt: '6:29 PM',
+  },
+  {
+    product: null,
+    avatar: null,
+    username: 'user3',
+    unread: 0,
+    lastMsg: 'hello, there',
+    updatedAt: '6:29 PM',
+  },
+  {
+    product: null,
+    avatar: null,
+    username: 'user4',
+    unread: 0,
+    lastMsg: 'hello, there',
+    updatedAt: '6:29 PM',
+  },
+  {
+    product: null,
+    avatar: null,
+    username: 'user5',
+    unread: 0,
+    lastMsg: 'hello, there',
+    updatedAt: '6:29 PM',
+  },
+];
+
+const Wrapper = styled.div`
   box-sizing: border-box;
   display: flex;
-  flex-direction: ${({ direction }) => direction || 'row'};
-  row-gap: ${({ rowGap }) => rowGap || 0};
-  column-gap: ${({ colGap }) => colGap || 0};
-  margin: ${(margin) => margin || 0};
-  padding: ${(padding) => padding || 0};
-
-  ${(xCenter) =>
-    xCenter &&
-    css`
-      justify-content: center;
-    `};
-  ${(yCenter) =>
-    yCenter &&
-    css`
-      align-items: center;
-    `};
+  margin: 16px;
+  height: calc(100% - 32px);
+  column-gap: 16px;
 `;
 
-const Container = styled.div`
-  background-color: ${PRIMARY_VAR_COLOR};
-  flex: 1;
-  max-width: ${(width) => width || 'fit-content'};
-  height: 100%;
-  border-radius: 4px;
+const Header = styled.h3`
+  padding: 16px;
+  font-size: 24px;
+  font-weight: bold;
 `;
-
-const Header = styled.div``;
 
 const Body = styled.div`
   padding: 16px;
@@ -79,28 +111,86 @@ const Body = styled.div`
   row-gap: 16px;
 `;
 
-const ChatRoomItem = styled.div`
-  background-color: pink;
-  display: flex;
-  justify-content: space-around;
+// chat
+const ChatList = styled.div`
+  padding: 16px;
+  background-color: #eeeeee;
+  height: 100%;
+  border-radius: 16px;
+  width: 480px;
+
+  .current {
+    background-color: ${PRIMARY_VAR_COLOR};
+  }
 `;
 
+const ChatItem = styled.div`
+  display: flex;
+  justify-content: space-around;
+  background: ${WHITE_COLOR};
+  border-radius: 8px;
+`;
+
+const Box = styled.div`
+  padding: ${({ padding }) => padding};
+  ${({ fill }) =>
+    fill &&
+    css`
+      flex: 1;
+    `}
+
+  ${({ center }) =>
+    center === 'true' &&
+    css`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
+`;
+
+//product
 const ProductImg = styled.div`
   border: 1px solid gray;
   width: 48px;
   height: 48px;
 `;
 
-const ChatContent = styled.div`
-  flex: 1;
-  background-color: orange;
+// userinfo
+const UserInfo = styled.div`
   display: flex;
-  flex-direction: column;
+  column-gap: 8px;
+  align-items: center;
 
-  div,
-  span {
+  div:last-child {
     flex: 1;
   }
 `;
 
-const LastMsgTime = styled.div``;
+const Avatar = styled.div`
+  width: 24px;
+  height: 24px;
+  border-radius: 24px;
+  background-color: lightgray;
+`;
+
+const Username = styled.div`
+  font-weight: bold;
+`;
+
+// message
+const LastMsg = styled.div`
+  margin-top: 8px;
+  height: 50%;
+`;
+
+const UpdatedTime = styled.div`
+  color: gray;
+`;
+
+// right - chat message
+const ChatMessage = styled.div`
+  background-color: #eeeeee;
+  flex: 1;
+  border-radius: 16px;
+  padding: 16px;
+`;
