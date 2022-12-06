aa3ff4fe981ae6375edc07c73f2534a6e799b7fd
ReturnReason <reasonz@daum.net>
2022-12-07 00:47:52 +0900

    feat(ProductView): API 연동 및 카드 컴포넌트 데이터 반영

--- front/src/pages/ProductView.jsx
+++ front/src/pages/ProductView.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 
 import Card from 'components/common/Card';
@@ -8,116 +9,60 @@ import SkeletonCard from 'components/common/SkeletonCard';
 import { useInView } from 'react-intersection-observer';
 import { useCallback } from 'react';
 
-// 임시 Mock 데이터
-const mockCardDatas = {
-  products: [
-    {
-      id: 0,
-      title: '타이틀1',
-      price: 123_456,
-      area: '서울시 동작구',
-      likes: 100,
-    },
-    {
-      id: 1,
-      title: '타이틀2',
-      price: 2_000,
-      area: '서울시 동작구',
-      likes: 1,
-    },
-    {
-      id: 2,
-      title: '타이틀3',
-      price: 440_000,
-      area: '서울시 동작구',
-      likes: 2000,
-    },
-    {
-      id: 3,
-      title: '타이틀4',
-      price: 12_000,
-      area: '서울시 동작구',
-      likes: 10,
-    },
-    {
-      id: 4,
-      title: '타이틀5',
-      price: 500_000,
-      area: '서울시 동작구',
-      likes: 10,
-    },
-    {
-      id: 5,
-      title: '타이틀6',
-      price: 20_000,
-      area: '서울시 동작구',
-      likes: 10,
-    },
-    {
-      id: 6,
-      title: '타이틀7',
-      price: 300_000,
-      area: '서울시 동작구',
-      likes: 2,
-    },
-    {
-      id: 7,
-      title: '타이틀8',
-      price: 12_000,
-      area: '서울시 동작구',
-      likes: 5,
-    },
-    {
-      id: 8,
-      title: '타이틀9',
-      price: '500,000',
-      area: '서울시 동작구',
-      likes: 10,
-    },
-    {
-      id: 9,
-      title: '타이틀10',
-      price: 12_000,
-      area: '서울시 동작구',
-      likes: 10,
-    },
-    {
-      id: 10,
-      title: '타이틀12',
-      price: 300_000,
-      area: '서울시 동작구',
-      likes: 2,
-    },
-    {
-      id: 11,
-      title: '타이틀13',
-      price: 12_000,
-      area: '서울시 동작구',
-      likes: 5,
-    },
-  ],
-};
+import { useGetSellAllQuery } from 'services/productApi';
+import Loading from 'components/common/Loading';
 
 const LOADING_CARD_COUNT = 4;
+const TITLE = Object.freeze({
+  sal: '신규 구매중 상품',
+  pal: '신규 판매중 상품',
+});
+const SUB_TITLE = Object.freeze({
+  sal: '구매하고 싶은 상품이 있다면 직접 구매글을 올려보세요!',
+  pal: '따끈따끈! 신규 등록된 판매중인 상품 목록이에요.',
+});
 
 export default function ProductView() {
-  const [cards, setCards] = useState(mockCardDatas.products);
+  const { type } = useParams();
+
+  const [currentPage, setCurrentPage] = useState('');
+  const [cards, setCards] = useState([]);
   const [pageNumber, setPageNumber] = useState(1);
-  const [isLoading, setIsLoading] = useState(false);
+  const [showSkeletonCard, setShowSkeletonCard] = useState(false);
+
   const [cardRef, inView] = useInView();
 
-  const [showSkeletonCard, setShowSkeletonCard] = useState(false);
+  const {
+    data: datas,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+  } = useGetSellAllQuery();
+
+  useEffect(() => {
+    if (type === 'sal') {
+      setCurrentPage('sal');
+    }
+    if (type === 'pal') {
+      setCurrentPage('pal');
+    }
+  }, [type]);
+
+  useEffect(() => {
+    if (datas) {
+      const { sellInfos } = datas;
+      sellInfos.forEach((data) => {
+        setCards((prev) => [...prev, data]);
+      });
+    }
+  }, [datas]);
 
   const getCards = useCallback(() => {
     // 서버에서 카드 데이터 받아오면 수정하기
-    setIsLoading(true);
-
     setTimeout(() => {
       setShowSkeletonCard(false);
-      setCards((prevCards) => [...prevCards, ...mockCardDatas.products]);
     }, 1000);
-
-    setIsLoading(false);
   }, [pageNumber]);
 
   useEffect(() => {
@@ -134,34 +79,55 @@ export default function ProductView() {
   }, [inView, isLoading]);
 
   return (
-    <Container>
-      <Header>
-        <Title>신규 판매중 상품</Title>
-        <SubText>따끈따끈! 판매중인 신규 상품 목록이에요.</SubText>
-      </Header>
-      <CardContainer>
-        {cards.map((product, idx) => {
-          const { id, title, price, likes, area } = product;
-          return (
-            <Card
-              key={`임시 키 입니다. id로 바꿀 부분 ${idx}`}
-              title={title}
-              price={price.toLocaleString()}
-              area={area}
-              likes={likes.toLocaleString()}
-            ></Card>
-          );
-        })}
-        {showSkeletonCard &&
-          Array(LOADING_CARD_COUNT)
-            .fill()
-            .map((_, idx) => {
-              return <SkeletonCard key={`SkeletonCard ${idx}`}></SkeletonCard>;
-            })}
-        <div ref={cardRef}></div>
-      </CardContainer>
-      <PlusButton />
-    </Container>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Container>
+          <Header>
+            <Title>{currentPage === 'sal' ? TITLE.sal : TITLE.pal}</Title>
+            <SubText>
+              {currentPage === 'sal' ? SUB_TITLE.sal : SUB_TITLE.pal}
+            </SubText>
+          </Header>
+          <CardContainer>
+            {cards.length > 0 &&
+              cards.map((product) => {
+                const {
+                  address,
+                  dbFileName,
+                  interestCnt,
+                  prodName,
+                  prodNo,
+                  prodPrice,
+                  tradState,
+                } = product;
+                return (
+                  <Card
+                    key={prodNo}
+                    // productImage={dbFileName}
+                    title={prodName}
+                    price={prodPrice.toLocaleString()}
+                    area={address}
+                    likes={interestCnt.toLocaleString()}
+                    state={tradState}
+                  ></Card>
+                );
+              })}
+            {showSkeletonCard &&
+              Array(LOADING_CARD_COUNT)
+                .fill()
+                .map((_, idx) => {
+                  return (
+                    <SkeletonCard key={`SkeletonCard ${idx}`}></SkeletonCard>
+                  );
+                })}
+            <div ref={cardRef}></div>
+          </CardContainer>
+          <PlusButton />
+        </Container>
+      )}
+    </>
   );
 }
 
