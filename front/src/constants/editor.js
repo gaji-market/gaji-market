73548c3b80e0b5a22176c9c925a4d24e2372d8de
ReturnReason <reasonz@daum.net>
2022-12-09 23:23:48 +0900

    chore(editor): 상수 문자 내용 수정

--- front/src/constants/editor.js
+++ front/src/constants/editor.js
@@ -6,7 +6,7 @@ export const TITLE = Object.freeze({
 });
 
 export const SUB_TITLE = Object.freeze({
-  addPal: '썸네일을 포함한 이미지를 1장 이상 업로드 해주세요.',
+  addPal: '썸네일을 포함한 이미지를 1장 이상 업로드 해주세요. (최대 5장)',
   updatePal: '',
   addSal:
     '이미지 업로드시 저작권에 주의해주세요. \n 무단사용에 따라 생긴 손실이나 결과에 대해 책임지지 않습니다.',
