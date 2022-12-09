92a57f2f15d0217f7e90d9dc4ead66583963a7c1
ReturnReason <reasonz@daum.net>
2022-12-10 07:51:25 +0900

    refactor(ProductView): 모달창 닫기 버튼 문구 수정

--- front/src/pages/ProductView.jsx
+++ front/src/pages/ProductView.jsx
@@ -89,7 +89,7 @@ export default function ProductView() {
         ref={modalRef}
         text='등록하기 페이지로 이동할까요?'
         leftBtnText='네! 좋아요.'
-        rightBtnText='조금 더 둘러볼래요!'
+        rightBtnText='아니요, 괜찮아요!'
       />
 
       <Container>
