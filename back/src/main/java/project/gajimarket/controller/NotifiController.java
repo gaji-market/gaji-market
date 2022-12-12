314bdbcb4802a0ac6b5c61509c8dcd64b7a89b19
HeeJooYoo <heejooyoo93@gmail.com>
2022-12-12 18:26:37 +0900

    refactor: Notification 기능 수정

--- back/src/main/java/project/gajimarket/controller/NotifiController.java
+++ back/src/main/java/project/gajimarket/controller/NotifiController.java
@@ -2,9 +2,12 @@ package project.gajimarket.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import project.gajimarket.service.NotifiService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RequiredArgsConstructor
@@ -13,7 +16,30 @@ import java.util.Map;
 @RequestMapping("/notifi")
 public class NotifiController {
 
-    public int addNotification(Map<String, Object> map) {
-        return 1;
+    private final NotifiService notifiService;
+
+    @PostMapping("/addNotifi")
+    public Map<String, Object> addNotification(@RequestBody Map<String, Object> requestMap) {
+
+        log.info("requestMap 확인 :: " + requestMap.toString());
+        requestMap.put("senderUserNo", 3); //user session
+
+        return notifiService.addNotification(requestMap);
+    }
+
+    @GetMapping("/getNotifiList")
+    public List<Map<String, Object>> getNotificationList(@RequestBody Map<String, Object> requestMap) {
+
+        return notifiService.getNotificationList(requestMap);
+    }
+
+    @GetMapping("/getNotifi")
+    public Map<String, Object> getNotification(@RequestBody Map<String, Object> map) {
+        return null;
+    }
+
+    @GetMapping("/getCheckCnt")
+    public Map<String, Object> getNotificationCheckCnt(@RequestBody Map<String, Object> map) {
+        return notifiService.getNotificationCheckCnt(map);
     }
 }
--- back/src/main/java/project/gajimarket/service/impl/NotifiServiceImpl.java
+++ back/src/main/java/project/gajimarket/service/impl/NotifiServiceImpl.java
@@ -3,8 +3,11 @@ package project.gajimarket.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import project.gajimarket.Utils;
+import project.gajimarket.dao.NotifiDAO;
 import project.gajimarket.service.NotifiService;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,9 +16,16 @@ import java.util.Map;
 @Service
 public class NotifiServiceImpl implements NotifiService {
 
+    private final NotifiDAO notifiDAO;
+
     @Override
-    public int addNotification(Map<String, Object> map) {
-        return 0;
+    public Map<String, Object> addNotification(Map<String, Object> map) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        int result = notifiDAO.insertNotification(map);
+        resultMap.put("result", Utils.resultMsg(result));
+
+        return resultMap;
     }
 
     @Override
@@ -24,12 +34,27 @@ public class NotifiServiceImpl implements NotifiService {
     }
 
     @Override
-    public List<Map<String, Object>> getNotificationList() {
-        return null;
+    public List<Map<String, Object>> getNotificationList(Map<String, Object> map) {
+        return notifiDAO.selectNotificationList(map);
     }
 
     @Override
-    public int updateNotification(Map<String, Object> map) {
-        return 0;
+    public Map<String, Object> updateNotification(Map<String, Object> map) {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        int result = notifiDAO.updateNotificationList(map);
+        resultMap.put("result", Utils.resultMsg(result));
+
+        return resultMap;
+    }
+
+    @Override
+    public int getNotificationCnt(Map<String, Object> map) {
+        return notifiDAO.selectNotificationCnt(map);
+    }
+
+    @Override
+    public Map<String, Object> getNotificationCheckCnt(Map<String, Object> map) {
+        return notifiDAO.selectNotificationCheckCnt(map);
     }
 }
