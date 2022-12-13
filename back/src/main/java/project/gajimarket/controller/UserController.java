cf08aa889890edb6a0ef0fca8fa1b5258e3cef34
Jisu-kkk <kjs964558@gmail.com>
2022-12-13 22:22:24 +0900

    refactor: 전체 오류 수정

--- back/src/main/java/project/gajimarket/controller/UserController.java
+++ back/src/main/java/project/gajimarket/controller/UserController.java
@@ -24,11 +24,12 @@ public class UserController {
     private final FileService fileService;
 
     // 아이디 중복확인
-    @GetMapping("/checkUserId")
+    @GetMapping("/checkUserId/{userId}")
     public Map<String, Object> checkUserId(@PathVariable String userId) throws Exception{
         Map<String, Object> resultMap = new HashMap<>();
         String result = "success";
         try {
+            System.out.println("userId : " + userId);
             int checkUserId = userService.checkUserId(userId);
             if (checkUserId > 0) {
                 result = "fail";
@@ -45,6 +46,7 @@ public class UserController {
     public Map<String,Object> signUp(@RequestBody UserDTO userDto) throws IOException {
         Map<String, Object> resultMap = new HashMap<>();
         try {
+            System.out.println("signUp userDto : " + userDto);
             int result = userService.insertUser(userDto);
             if (result > 0) {
                 resultMap.put("result", "success");
@@ -62,31 +64,32 @@ public class UserController {
     public Map<String, Object> signIn(@RequestBody UserDTO userDto, HttpServletRequest request) throws IOException {
         Map<String, Object> resultMap = new HashMap<>();
         Map<String, Object> param = new HashMap<>();
+        String result = "fail";
         try {
             String userId = userDto.getUserId();
             String userPwd = userDto.getUserPwd();
-            System.out.println("userController signIn");
-            System.out.println("userDto : " + userDto);
-            System.out.println("userId : " + userId);
-            System.out.println("userPwd : " + userPwd);
+            // 받은 id, pwd
+            System.out.println("userId : " + userId + " ::: userPwd : " + userPwd);
 
-            if (userId != null && "".equals(userId)){
+            if (userId != null && !"".equals(userId)){
                 param.put("userId", userId);
             }
-            if (userPwd != null && "".equals(userPwd)){
+            if (userPwd != null && !"".equals(userPwd)){
                 param.put("userPwd", userPwd);
             }
 
             UserDTO selectUser = userService.selectUser(param);
             if (selectUser != null) {
+                result = "success";
                 request.getSession().setAttribute("userInfo", selectUser);
                 resultMap.put("userInfo", selectUser);
             }
-
+            // 저장하는 UserDTO
+            System.out.println("signIn session userInfo : " + request.getSession().getAttribute("userInfo"));
+            resultMap.put("result", result);
         }catch (Exception e) {
             System.out.println("UserController signIn : " + e);
         }
-
         return resultMap;
     }
 
@@ -94,6 +97,8 @@ public class UserController {
     @GetMapping("/myPage")
     public Map<String, Object> myPage(HttpServletRequest request) throws IOException {
         Map<String, Object> resultMap = new HashMap<>();
+        // 세션에 있는 UserDTO
+        System.out.println("myPage session UserDto : " + (UserDTO)request.getSession().getAttribute("userInfo"));
         UserDTO userDto = (UserDTO) request.getSession().getAttribute("userInfo");
         String result = "fail";
         try {
@@ -116,6 +121,8 @@ public class UserController {
         try {
             result.put("result", "fail");
 
+            // 받은 UserDTO
+            System.out.println("UserDTO : " + userDto);
             if (userDto != null) {
                 int update = userService.updateUser(userDto);
                 if (update > 0) {
@@ -125,10 +132,12 @@ public class UserController {
             }
 
             // 수정성공시 session에 담음
-            if (!"false".equals(result.get("result"))) {
+            if (!"fail".equals(result.get("result"))) {
                 request.getSession().setAttribute("userInfo", userDto);
                 result.put("userInfo", userService.selectUser(param));
             }
+            // 세션에 있는 userDTO
+            System.out.println("userUpdate session UserDto : " + (UserDTO)request.getSession().getAttribute("userInfo"));
 
         }catch (Exception e) {
             System.out.println("UserController userUpdate : " + e);
@@ -137,16 +146,17 @@ public class UserController {
     }
 
     // 알림 수정
-    @GetMapping("/updateNtfct")
-    public Map<String, Object> updateNtfct(@PathVariable String gubun, String useYn, HttpServletRequest request) {
+    @GetMapping("/updateNtfct/{gubun}/{useYn}")
+    public Map<String, Object> updateNtfct(@PathVariable String gubun, @PathVariable String useYn, HttpServletRequest request) {
         Map<String, Object> param = new HashMap<>();
         Map<String, Object> resultMap = new HashMap<>();
         String result = "fail";
         try {
             UserDTO user = (UserDTO) request.getSession().getAttribute("userInfo");
+            System.out.println("updateNtfct session userInfo : " + user);
 
             // gubun ( 0: 채팅, 1: 댓글, 2: 좋아요 )
-            if (gubun != null && "".equals(gubun)){
+            if (gubun != null && !"".equals(gubun)){
                 if (useYn != null && "".equals(useYn)){
                     param.put("gubun", gubun);
                     param.put("useYn", useYn);
@@ -156,6 +166,8 @@ public class UserController {
                 param.put("userNo", user.getUserNo());
             }
 
+            // 수정하는 Map 확인
+            System.out.println("updateNtfct param : " + param);
             int updateNtfctResult = userService.updateNtfct(param);
             if (updateNtfctResult > 0) {
                 result = "success";
@@ -184,13 +196,17 @@ public class UserController {
         return result;
     }
 
-    @GetMapping("/outUser")
-    public Map<String, Object> outUser(@PathVariable int userNo, HttpServletRequest request) {
+    // 탈퇴
+    @PostMapping("/outUser")
+    public Map<String, Object> outUser(HttpServletRequest request) {
         Map<String, Object> resultMap = new HashMap<>();
         String result = "fail";
         try {
-            if (userNo > 0) {
-                int updateOutUser = userService.updateOutUser(userNo);
+            UserDTO userDto = (UserDTO) request.getSession().getAttribute("userInfo");
+            System.out.println("outUser session userInfo : " + userDto);
+
+            if (userDto != null) {
+                int updateOutUser = userService.updateOutUser(userDto.getUserNo());
                 if (updateOutUser > 0) {
                     result = "success";
                     request.getSession().setAttribute("userInfo", null);
