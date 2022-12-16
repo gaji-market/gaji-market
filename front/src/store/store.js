949a4e02567bc6433e511ef4a479a4ea52db3a4c
vjvl95 <vjvl95@naver.com>
2022-12-16 13:02:00 +0900

    refactor: 마이페이지 라우팅 추가

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -2,11 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import tempSlice from './tempSlice';
 
 import { signUpApi } from 'services/signUpApi';
+import { productApi } from 'services/productApi';
 
 export const store = configureStore({
   reducer: {
     temp: tempSlice,
     [signUpApi.reducerPath]: signUpApi.reducer,
+    [productApi.reducerPath]: productApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(signUpApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([signUpApi.middleware, productApi.middleware]),
 });
