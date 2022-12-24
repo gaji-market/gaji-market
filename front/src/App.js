9fe6916af5e146d2d9bbef482e941c88d13b8a3e
ReturnReason <reasonz@daum.net>
2022-12-25 02:47:08 +0900

    feat(scrollTop): 페이지 이동시 스크롤이 최상단에 위치할 수 있도록 구현

--- front/src/App.js
+++ front/src/App.js
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 import Router from 'routes';
+import ScrollTop from 'routes/scrollTop';
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollTop />
         <Router />
       </BrowserRouter>
     </Provider>
