202d1ae661bade9fd13518b0f7a556466f7c4e54
vjvl95 <vjvl95@naver.com>
2023-01-02 19:08:07 +0900

    feat: 토스트창을 위한 redux 설정

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,12 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import tempSlice from './tempSlice';
-
+import toastSlice from './toastSlice';
 import { signUpApi } from 'services/signUpApi';
 import { productApi } from 'services/productApi';
 
 export const store = configureStore({
   reducer: {
     temp: tempSlice,
+    toast: toastSlice,
     [signUpApi.reducerPath]: signUpApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
   },
---
+++ front/src/store/toastSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  toasts: [],
+};
+
+const toastSlice = createSlice({
+  name: 'toast',
+  initialState,
+  reducers: {
+    addToast: (state, action) => {
+      state.toasts.push(action.payload);
+    },
+
+    removeToast: (state, action) => {
+      state.toasts = state.toasts.filter((toast) => {
+        return toast.id !== action.payload;
+      });
+    },
+  },
+});
+
+export const { addToast, removeToast } = toastSlice.actions;
+
+export default toastSlice.reducer;
