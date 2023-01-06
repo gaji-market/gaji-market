809936d6cc02286171c66f37b22d2176b91b7362
Sunny <in.wise.nature@gmail.com>
2023-01-06 20:05:27 +0900

    fix: resolve conflict

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+
 import tempSlice from './tempSlice';
 import toastSlice from './toastSlice';
+import sessionSlice from './sessionSlice';
+
 import { signUpApi } from 'services/signUpApi';
 import { productApi } from 'services/productApi';
 import { chatApi } from 'services/chatApi';
@@ -9,6 +12,7 @@ export const store = configureStore({
   reducer: {
     temp: tempSlice,
     toast: toastSlice,
+    session: sessionSlice,
     [signUpApi.reducerPath]: signUpApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
     [chatApi.reducerPath]: chatApi.reducer,
