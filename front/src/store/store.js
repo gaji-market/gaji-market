97b3da3b1d0d534a346d248f6bc2337519ea70ab
Sunny <in.wise.nature@gmail.com>
2023-01-07 15:59:51 +0900

    feat: add queris to alarm api

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -7,6 +7,7 @@ import sessionSlice from './sessionSlice';
 import { signUpApi } from 'services/signUpApi';
 import { productApi } from 'services/productApi';
 import { chatApi } from 'services/chatApi';
+import { alarmApi } from 'services/alarmApi';
 
 export const store = configureStore({
   reducer: {
@@ -16,11 +17,13 @@ export const store = configureStore({
     [signUpApi.reducerPath]: signUpApi.reducer,
     [productApi.reducerPath]: productApi.reducer,
     [chatApi.reducerPath]: chatApi.reducer,
+    [alarmApi.reducerPath]: alarmApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat([
       signUpApi.middleware,
       productApi.middleware,
       chatApi.middleware,
+      alarmApi.middleware,
     ]),
 });
