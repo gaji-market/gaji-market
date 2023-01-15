af959ecd1fa9ab1a3acc787721a7d6ec3f9d436a
ReturnReason <reasonz@daum.net>
2023-01-16 02:00:18 +0900

    refactor: 테스트 파일들 삭제

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,6 +1,5 @@
 export { default as Home } from './Home';
 export { default as Login } from './Login';
-export { default as SliceTest } from './SliceTest';
 export { default as SignUp } from './SignUp';
 export { default as ProductView } from './ProductView';
 export { default as ProductDetailView } from './ProductDetailView';
