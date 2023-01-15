0842b1a7eed5b5b0fceb2787cfa6174f10e69474
vjvl95 <vjvl95@naver.com>
2023-01-15 13:59:54 +0900

    임시 커밋: 이유님 이거 보세요

--- front/src/pages/ProductView.jsx
+++ front/src/pages/ProductView.jsx
@@ -177,17 +177,18 @@ export default function ProductView() {
     const tagName = e.target.tagName;
     const interestIconTag = ['path', 'svg'];
 
-    if (interestIconTag.includes(tagName)) {
-      (async () => {
-        try {
-          await changeInterestCountMutation(prodNo);
-        } catch (error) {
-          console.error(error);
-        }
-      })();
-    } else {
-      navigate(`/products/${param}/detail/${prodNo}`);
-    }
+    // if (interestIconTag.includes(tagName)) {
+    //   (async () => {
+    //     try {
+    //       const res = await changeInterestCountMutation(prodNo).unwrap();
+    //       console.log(res);
+    //     } catch (error) {
+    //       console.error(error);
+    //     }
+    //   })();
+    // } else {
+    //   navigate(`/products/${param}/detail/${prodNo}`);
+    // }
   };
 
   if (getAllProducts?.[param]?.isError) {
@@ -238,6 +239,7 @@ export default function ProductView() {
                   }
                   title={prodName}
                   price={prodPrice.toLocaleString()}
+                  prodNo={prodNo}
                   area={address}
                   likes={interestCnt.toLocaleString()}
                   isInterest={interestYN}
