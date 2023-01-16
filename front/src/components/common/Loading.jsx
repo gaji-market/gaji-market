0d1617083f028e217de0de560bc5206148d42d6a
vjvl95 <vjvl95@naver.com>
2023-01-17 02:04:43 +0900

    refactor: 가지로딩 이미지 밑에 로딩 글자 추가

--- front/src/components/common/Loading.jsx
+++ front/src/components/common/Loading.jsx
@@ -1,11 +1,32 @@
 import styled, { keyframes, css } from 'styled-components';
 import gajiImg from 'assets/BasicLogo.svg';
+import { PRIMARY_COLOR } from './commonColor';
+const TEXT_DELAY = [
+  ['L', '0.1s'],
+  ['o', '0.2s'],
+  ['a', '0.3s'],
+  ['d', '0.4s'],
+  ['i', '0.5s'],
+  ['n', '0.6s'],
+  ['g', '0.7s'],
+  ['.', '0.8s'],
+  ['.', '0.9s'],
+  ['.', '1.0s'],
+  ['.', '1.1s'],
+];
 export default function Loading() {
   return (
     <Container>
-      <LoadingIMG left='60px'></LoadingIMG>
-      <LoadingIMG left='0px'></LoadingIMG>
-      <LoadingIMG left='-60px'></LoadingIMG>
+      <IMG>
+        <LoadingIMG animationName={moveImageFirst} left='60px'></LoadingIMG>
+        <LoadingIMG animationName={moveImageMiddle} left='0px'></LoadingIMG>
+        <LoadingIMG animationName={moveImageLast} left='-60px'></LoadingIMG>
+      </IMG>
+      <Text>
+        {TEXT_DELAY.map((v) => {
+          return <LoadingText delay={v[1]}>{v[0]}</LoadingText>;
+        })}
+      </Text>
     </Container>
   );
 }
@@ -45,23 +66,37 @@ const moveImageLast = keyframes`
       transform:translateX(55px)  rotate(0deg) 
     }    
 `;
-
+const TextAnimation = keyframes`
+  0%{
+    transform:translateY(0px) 
+  }
+  50%{
+    transform:translateY(30px) 
+  }
+  100%{
+    transform:translateY(0px) 
+  }
+`;
+const IMG = styled.div`
+  display: flex;
+`;
+const Text = styled.div`
+  font-size: 50px;
+  color: ${PRIMARY_COLOR};
+  font-weight: 800;
+  display: flex;
+`;
+const LoadingText = styled.div`
+  margin-left: 5px;
+  animation: ${TextAnimation} 2s infinite;
+  animation-delay: ${(props) => props.delay};
+`;
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-  div {
-    &:first-child {
-      animation: ${moveImageFirst} 2s infinite;
-    }
-    &:nth-child(2) {
-      animation: ${moveImageMiddle} 2s infinite;
-    }
-    &:last-child {
-      animation: ${moveImageLast} 2s infinite;
-    }
-  }
 `;
 
 const LoadingIMG = styled.div`
@@ -69,6 +104,6 @@ const LoadingIMG = styled.div`
   height: 100px;
   position: relative;
   background-image: url(${gajiImg});
-  transform-origin: 50% 50%;
   left: ${({ left }) => left};
+  animation: ${(props) => props.animationName} 2s infinite;
 `;
