82bf4d568df2cf5aa7862f650b250cd7b2527d67
ReturnReason <reasonz@daum.net>
2023-01-30 10:03:31 +0900

    design(Layout_WithoutAppBar): 스타일 추가

--- front/src/layouts/Layout_WithoutAppBar.jsx
+++ front/src/layouts/Layout_WithoutAppBar.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+import { PRIMARY_COLOR } from 'components/common/commonColor';
+
 export default function Layout_WithoutAppBar() {
   return (
     <Container>
@@ -9,7 +12,46 @@ export default function Layout_WithoutAppBar() {
   );
 }
 
+const fade = keyframes`
+  0% {
+    opacity: .1;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: .1;
+  }
+`;
+
 const Container = styled.div`
-  width: 1200px;
+  width: 100vw;
   margin: 0 auto;
+  display: flex;
+  jusity-contents: center;
+  align-item: center;
+  overflow: hidden;
+
+  ::before {
+    content: '';
+    position: absolute;
+    z-index: -15;
+    display: block;
+    width: 100%;
+    height: 100%;
+    background: ${PRIMARY_COLOR};
+    top: 0;
+  }
+
+  ::after {
+    content: '';
+    background: url('https://raw.githubusercontent.com/gaji-market/gaji-market/ef62c8bc4463b0a90b4b80770e275279f713a36c/front/src/assets/bg_pattern.png')
+      0px 0px;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: -10;
+    opacity: 0.1;
+    animation: ${fade} 3s 1s infinite linear;
+  }
 `;
