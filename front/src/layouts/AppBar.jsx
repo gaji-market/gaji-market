b6ee17ad8590031e4e3794343a04f59e959b61b3
ReturnReason <reasonz@daum.net>
2023-02-02 08:16:28 +0900

    feat(AppBar): 앱 바 서치 아이콘 추가 및 스타일 수정

--- front/src/layouts/AppBar.jsx
+++ front/src/layouts/AppBar.jsx
@@ -36,6 +36,7 @@ import {
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { FaBell, FaUserCircle } from 'react-icons/fa';
 import { AiFillMessage, AiFillHeart } from 'react-icons/ai';
+import { GrFormSearch } from 'react-icons/gr';
 
 export default function AppBar() {
   const { userId, userNo, isLoggedIn } = useSelector(selectSession);
@@ -136,205 +137,213 @@ export default function AppBar() {
 
       <StyledWrapper className='fade-in'>
         <ItemGroup>
-          <MenuBtn onClick={menubarHandler}>
-            <GiHamburgerMenu size={24} color={PRIMARY_COLOR} />
-          </MenuBtn>
-          <GradationLogo
-            height='60px'
-            onClick={() => navigate('/')}
-            style={{ cursor: 'pointer' }}
-          />
-        </ItemGroup>
-        <ItemGroup>
-          {toggles.productSwitch && (
-            <ToggleSwitch
-              defaultValue={type === 'pal'}
-              on={{
-                name: '팔래요',
-                handler: () => navigate('products/pal'),
-              }}
-              off={{
-                name: '살래요',
-                handler: () => navigate('products/sal'),
-              }}
+          <ItemGroupLeft>
+            <MenuBtn onClick={menubarHandler}>
+              <GiHamburgerMenu size={24} color={PRIMARY_COLOR} />
+            </MenuBtn>
+            <GradationLogo
+              height='60px'
+              onClick={() => navigate('/')}
+              style={{ cursor: 'pointer' }}
             />
-          )}
-          <Search
-            type='search'
-            ref={searchRef}
-            placeholder='상품명을 입력하세요'
-            onKeyDown={keydownHandler}
-          />
-          {isLoggedIn ? (
-            <>
-              {(toggles.alarm || toggles.userId) && (
-                <BlurContainer onClick={blurHandler} />
-              )}
-              <Alarm aria-expanded={toggles.alarm}>
-                <Toggle
-                  onClick={() => [
-                    getNotiListHandler(),
-                    setToggles((prev) => ({ ...prev, alarm: !prev.alarm })),
-                  ]}
-                >
-                  <FaBell size={24} color={PRIMARY_COLOR} />
-                  {counts.intstCheckCnt + counts.chatCheckCnt > 0 && (
-                    <CountBadge
-                      count={counts.intstCheckCnt + counts.chatCheckCnt}
-                      style={{ marginLeft: '-16px' }}
-                    />
+          </ItemGroupLeft>
+          <ItemGroupRight>
+            {toggles.productSwitch && (
+              <ToggleSwitch
+                defaultValue={type === 'pal'}
+                on={{
+                  name: '팔래요',
+                  handler: () => navigate('products/pal'),
+                }}
+                off={{
+                  name: '살래요',
+                  handler: () => navigate('products/sal'),
+                }}
+              />
+            )}
+            <SearchIcon onClick={() => searchRef.current.focus()} />
+            <Search
+              type='search'
+              ref={searchRef}
+              placeholder='상품명을 입력하세요'
+              onKeyDown={keydownHandler}
+            />
+            {isLoggedIn ? (
+              <>
+                {(toggles.alarm || toggles.userId) && (
+                  <BlurContainer onClick={blurHandler} />
+                )}
+                <Alarm aria-expanded={toggles.alarm}>
+                  <Toggle
+                    onClick={() => [
+                      getNotiListHandler(),
+                      setToggles((prev) => ({ ...prev, alarm: !prev.alarm })),
+                    ]}
+                  >
+                    <FaBell size={24} color={PRIMARY_COLOR} />
+                    {counts.intstCheckCnt + counts.chatCheckCnt > 0 && (
+                      <CountBadge
+                        count={counts.intstCheckCnt + counts.chatCheckCnt}
+                        style={{ marginLeft: '-16px' }}
+                      />
+                    )}
+                  </Toggle>
+                  {toggles.alarm && (
+                    <AlarmContainer id='app-alarm-container'>
+                      <Title>알림</Title>
+                      <Tabs>
+                        <Tab
+                          aria-current={currentTab === '채팅' && 'page'}
+                          onClick={() => [
+                            setCurrentTab('채팅'),
+                            getNotiListHandler(),
+                          ]}
+                        >
+                          <span>채팅</span>
+                          {counts.chatCheckCnt > 0 && (
+                            <CountBadge
+                              count={counts.chatCheckCnt}
+                              style={{ marginLeft: '8px' }}
+                            />
+                          )}
+                        </Tab>
+                        <Tab
+                          aria-current={currentTab === '좋아요' && 'page'}
+                          onClick={() => [
+                            setCurrentTab('좋아요'),
+                            getNotiListHandler('좋아요'),
+                          ]}
+                        >
+                          <span>좋아요</span>
+                          {counts.intstCheckCnt > 0 && (
+                            <CountBadge
+                              count={counts.intstCheckCnt}
+                              style={{ marginLeft: '8px' }}
+                            />
+                          )}
+                        </Tab>
+                      </Tabs>
+                      <FloatRight>
+                        <span
+                          onClick={() => [
+                            navigate(
+                              currentTab === '채팅' ? '/chat' : '/myPage'
+                            ),
+                            blurHandler(),
+                          ]}
+                        >
+                          전체보기
+                        </span>
+                      </FloatRight>
+                      <AlarmBody>
+                        {notificationInfos.length === 0 && (
+                          <EmptyMsg>알림이 없습니다.</EmptyMsg>
+                        )}
+                        {notificationInfos.map((item, idx) => (
+                          <NotiItem
+                            key={idx}
+                            aria-checked={item.checkYn === 'Y'}
+                          >
+                            <div className='strong-text'>{item.message}</div>
+                            <div>{item.nickname}</div>
+                            <span>{item.regDate.split('T')[0]}</span>
+                            {currentTab === '채팅' ? (
+                              <AiFillMessage
+                                id={item.notifiNo}
+                                className='msg-icon'
+                                aria-checked={item.checkYn === 'Y'}
+                                color={
+                                  item.checkYn === 'Y' ? GRAY_COLOR : RED_COLOR
+                                }
+                                onClick={() =>
+                                  item.checkYn === 'N' && [
+                                    checkNotiHandler(item.notifiNo),
+                                    getNotiListHandler(currentTab),
+                                  ]
+                                }
+                              />
+                            ) : (
+                              <AiFillHeart
+                                id={item.notifiNo}
+                                className='msg-icon'
+                                aria-checked={item.checkYn === 'Y'}
+                                color={
+                                  item.checkYn === 'Y' ? GRAY_COLOR : RED_COLOR
+                                }
+                                onClick={() =>
+                                  item.checkYn === 'N' && [
+                                    checkNotiHandler(item.notifiNo),
+                                    getNotiListHandler(currentTab),
+                                  ]
+                                }
+                              />
+                            )}
+                            {item.checkYn === 'N' && (
+                              <ReactTooltip
+                                anchorId={item.notifiNo}
+                                place='top'
+                                content='읽음으로 표시'
+                              />
+                            )}
+                          </NotiItem>
+                        ))}
+                      </AlarmBody>
+                    </AlarmContainer>
                   )}
-                </Toggle>
-                {toggles.alarm && (
-                  <AlarmContainer id='app-alarm-container'>
-                    <Title>알림</Title>
-                    <Tabs>
-                      <Tab
-                        aria-current={currentTab === '채팅' && 'page'}
+                </Alarm>
+                <UserItem>
+                  <Toggle
+                    onClick={() =>
+                      setToggles((prev) => ({ ...prev, userId: !prev.userId }))
+                    }
+                  >
+                    <FaUserCircle size={24} color={GRAY_COLOR} />
+                    <span>{userId || ''}</span>
+                  </Toggle>
+                  {toggles.userId && (
+                    <UserIdDropdown>
+                      <DropdownItem
                         onClick={() => [
-                          setCurrentTab('채팅'),
-                          getNotiListHandler(),
+                          navigate('/mypage'),
+                          setToggles(initToggles),
                         ]}
                       >
-                        <span>채팅</span>
-                        {counts.chatCheckCnt > 0 && (
-                          <CountBadge
-                            count={counts.chatCheckCnt}
-                            style={{ marginLeft: '8px' }}
-                          />
-                        )}
-                      </Tab>
-                      <Tab
-                        aria-current={currentTab === '좋아요' && 'page'}
+                        마이페이지
+                      </DropdownItem>
+                      <DropdownItem
                         onClick={() => [
-                          setCurrentTab('좋아요'),
-                          getNotiListHandler('좋아요'),
+                          modalRef.current?.showModal(),
+                          setToggles(initToggles),
                         ]}
                       >
-                        <span>좋아요</span>
-                        {counts.intstCheckCnt > 0 && (
-                          <CountBadge
-                            count={counts.intstCheckCnt}
-                            style={{ marginLeft: '8px' }}
-                          />
-                        )}
-                      </Tab>
-                    </Tabs>
-                    <FloatRight>
-                      <span
-                        onClick={() => [
-                          navigate(currentTab === '채팅' ? '/chat' : '/myPage'),
-                          blurHandler(),
-                        ]}
-                      >
-                        전체보기
-                      </span>
-                    </FloatRight>
-                    <AlarmBody>
-                      {notificationInfos.length === 0 && (
-                        <EmptyMsg>알림이 없습니다.</EmptyMsg>
-                      )}
-                      {notificationInfos.map((item, idx) => (
-                        <NotiItem key={idx} aria-checked={item.checkYn === 'Y'}>
-                          <div className='strong-text'>{item.message}</div>
-                          <div>{item.nickname}</div>
-                          <span>{item.regDate.split('T')[0]}</span>
-                          {currentTab === '채팅' ? (
-                            <AiFillMessage
-                              id={item.notifiNo}
-                              className='msg-icon'
-                              aria-checked={item.checkYn === 'Y'}
-                              color={
-                                item.checkYn === 'Y' ? GRAY_COLOR : RED_COLOR
-                              }
-                              onClick={() =>
-                                item.checkYn === 'N' && [
-                                  checkNotiHandler(item.notifiNo),
-                                  getNotiListHandler(currentTab),
-                                ]
-                              }
-                            />
-                          ) : (
-                            <AiFillHeart
-                              id={item.notifiNo}
-                              className='msg-icon'
-                              aria-checked={item.checkYn === 'Y'}
-                              color={
-                                item.checkYn === 'Y' ? GRAY_COLOR : RED_COLOR
-                              }
-                              onClick={() =>
-                                item.checkYn === 'N' && [
-                                  checkNotiHandler(item.notifiNo),
-                                  getNotiListHandler(currentTab),
-                                ]
-                              }
-                            />
-                          )}
-                          {item.checkYn === 'N' && (
-                            <ReactTooltip
-                              anchorId={item.notifiNo}
-                              place='top'
-                              content='읽음으로 표시'
-                            />
-                          )}
-                        </NotiItem>
-                      ))}
-                    </AlarmBody>
-                  </AlarmContainer>
-                )}
-              </Alarm>
-              <UserItem>
-                <Toggle
-                  onClick={() =>
-                    setToggles((prev) => ({ ...prev, userId: !prev.userId }))
-                  }
+                        로그아웃
+                      </DropdownItem>
+                    </UserIdDropdown>
+                  )}
+                </UserItem>
+              </>
+            ) : (
+              <>
+                <Button
+                  size='sm'
+                  padding='0'
+                  height='32px'
+                  isOutline
+                  onClick={() => navigate('/login')}
                 >
-                  <FaUserCircle size={24} color={GRAY_COLOR} />
-                  <span>{userId || ''}</span>
-                </Toggle>
-                {toggles.userId && (
-                  <UserIdDropdown>
-                    <DropdownItem
-                      onClick={() => [
-                        navigate('/mypage'),
-                        setToggles(initToggles),
-                      ]}
-                    >
-                      마이페이지
-                    </DropdownItem>
-                    <DropdownItem
-                      onClick={() => [
-                        modalRef.current?.showModal(),
-                        setToggles(initToggles),
-                      ]}
-                    >
-                      로그아웃
-                    </DropdownItem>
-                  </UserIdDropdown>
-                )}
-              </UserItem>
-            </>
-          ) : (
-            <>
-              <Button
-                size='sm'
-                padding='0'
-                height='32px'
-                isOutline
-                onClick={() => navigate('/login')}
-              >
-                로그인
-              </Button>
-              <Button
-                size='sm'
-                padding='0'
-                height='32px'
-                onClick={() => navigate('/signup')}
-              >
-                회원가입
-              </Button>
-            </>
-          )}
+                  로그인
+                </Button>
+                <Button
+                  size='sm'
+                  padding='0'
+                  height='32px'
+                  onClick={() => navigate('/signup')}
+                >
+                  회원가입
+                </Button>
+              </>
+            )}
+          </ItemGroupRight>
         </ItemGroup>
       </StyledWrapper>
       <Spacer />
@@ -361,7 +370,6 @@ const StyledWrapper = styled.div`
   display: flex;
   column-gap: 16px;
   align-items: center;
-  justify-content: space-between;
   padding: 16px;
   height: 56px;
   box-sizing: border-box;
@@ -370,9 +378,25 @@ const StyledWrapper = styled.div`
 `;
 
 const ItemGroup = styled.div`
+  min-width: 90vw;
   display: flex;
   column-gap: 16px;
   align-items: center;
+  justify-content: space-between;
+  margin: 0 auto;
+`;
+
+const ItemGroupLeft = styled.div`
+  display: flex;
+`;
+
+const ItemGroupRight = styled.div`
+  display: flex;
+  align-items: center;
+
+  button {
+    margin-left: 10px;
+  }
 `;
 
 const MenuBtn = styled.button`
@@ -396,10 +420,20 @@ const Search = styled.input`
   padding: 0 8px 0 16px;
   border: 1px solid ${GRAY_COLOR};
   transition: all 0.2s;
+  margin-right: 10px;
 
   &:focus {
     outline: none;
     border: 1px solid ${PRIMARY_COLOR};
+  }
+`;
+
+const SearchIcon = styled(GrFormSearch)`
+  font-size: 30px;
+  cursor: pointer;
+
+  path {
+    stroke: ${GRAY_COLOR};
   }
 `;
 
@@ -415,6 +449,7 @@ const Toggle = styled.div`
 
 const Alarm = styled.div`
   position: relative;
+  margin-right: 10px;
 `;
 
 const AlarmContainer = styled.div`
@@ -427,9 +462,9 @@ const AlarmContainer = styled.div`
   top: 38px;
   right: -16px;
   border-radius: 8px;
-  border: 3px solid ${PRIMARY_COLOR};
   background-color: white;
-  box-shadow: 3px 3px 30px ${GRAY_COLOR};
+  border: 1px solid ${GRAY_COLOR};
+  box-shadow: 2px 2px 10px #eeeeee50;
   padding: 16px;
 
   a {
@@ -439,7 +474,7 @@ const AlarmContainer = styled.div`
 `;
 
 const Title = styled.h2`
-  font-size: 28px;
+  font-size: 20px;
   font-weight: bolder;
   margin: 16px;
 `;
@@ -452,7 +487,7 @@ const Tabs = styled.div`
 
 const Tab = styled.div`
   flex: 1;
-  font-size: 20px;
+  font-size: 16px;
   font-weight: bold;
   text-align: center;
   height: 32px;
@@ -462,14 +497,14 @@ const Tab = styled.div`
   justify-content: center;
 
   &[aria-current='page'] {
-    border-bottom: 4px solid ${PRIMARY_COLOR};
+    border-bottom: 1px solid ${PRIMARY_COLOR};
   }
 
   &:hover {
     cursor: pointer;
 
     span {
-      font-size: 24px;
+      font-size: 18px;
       color: ${PRIMARY_COLOR};
     }
   }
